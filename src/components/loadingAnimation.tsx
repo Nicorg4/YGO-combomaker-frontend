03e1c3a12f785bce840e9383bd0f9cc73a3e9fc6
Nicorg4 <nicolasgota@hotmail.com>
2025-11-24 08:03:03 -0300

    refactor: Simplify loadingAnimation component by removing redundant image picking function and improving state management

--- src/components/loadingAnimation.tsx
+++ src/components/loadingAnimation.tsx
@@ -1,19 +1,31 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import React from "react";
 import Puzzle from "../../public/images/puzzle.png";
 import Kuriboh from "../../public/images/kuriboh.png";
 import Crimson from "../../public/images/crimson.png";
 import mainWallpaper from "../../public/images/main_wallpaper.png";
+import { usePathname } from "next/navigation";
 
-const pickRandomImage = () => {
-  const images = [Puzzle, Kuriboh, Crimson];
-  const randomIndex = Math.floor(Math.random() * images.length);
-  return images[randomIndex];
+const IMAGES = [Puzzle, Kuriboh, Crimson];
+
+type Props = {
+  randomizeOnRouteChange?: boolean;
 };
 
-const LoadingAnimation = () => {
+export default function LoadingAnimation({ randomizeOnRouteChange = false }: Props) {
+  const [image, setImage] = useState(() => {
+    return IMAGES[Math.floor(Math.random() * IMAGES.length)];
+  });
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (!randomizeOnRouteChange) return;
+    setImage(IMAGES[Math.floor(Math.random() * IMAGES.length)]);
+  }, [pathname, randomizeOnRouteChange]);
+
   return (
     <div
       className="fixed inset-0 z-50 bg-slate-700 flex justify-center items-center"
@@ -26,12 +38,10 @@ const LoadingAnimation = () => {
       <Image
         width={200}
         height={200}
-        src={pickRandomImage()}
+        src={image}
         alt="loading"
         className="pulsating"
       />
     </div>
   );
-};
-
-export default LoadingAnimation;
+}
